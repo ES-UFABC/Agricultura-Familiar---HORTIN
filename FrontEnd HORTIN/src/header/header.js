ba1e63b930080ca791166c490e4bd0d895b15f80
HenriqueEug <henrique.10eugenio@gmail.com>
2022-04-18 20:52:28 -0300

    alteracao datatable

--- FrontEnd HORTIN/src/header/header.js
+++ FrontEnd HORTIN/src/header/header.js
@@ -4,11 +4,18 @@ $(document).ready(function() {
     //     var title = $(this).text();
     //     $(this).html( '<input type="text" size="10" placeholder="'+title+'" /input>' );
     // } );
-
+    var vendedor = localStorage.getItem("id_usuario");
     window.table = $("#tabelaVendedor").DataTable({
         paging: true,
+        pagingType : "full",
         dom: 'Bfrtip',
         colReorder: true,
+        searching : true,
+        "processing" : true,
+        "ajax" : {
+            "url" : "http://localhost:8080/produto/vendedor/"+ vendedor.toString(),
+            "type" : "GET"
+        },
             buttons: [
                 {
                     extend: 'colvis',
@@ -28,45 +35,7 @@ $(document).ready(function() {
         scrollY: "400px",
         scrollX: "0px",
         scrollCollapse: true,
-        language: {
-            "sEmptyTable": "Nenhum registro encontrado",
-            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
-            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
-            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
-            "sInfoPostFix": "",
-            "sInfoThousands": ".",
-            "sLengthMenu": "_MENU_ resultados por página",
-            "sLoadingRecords": "Carregando...",
-            "sProcessing": "Processando...",
-            "sZeroRecords": "Nenhum registro encontrado",
-            "sSearch": "Pesquisar",
-            "oPaginate": {
-                "sNext": "Próximo",
-                "sPrevious": "Anterior",
-                "sFirst": "Primeiro",
-                "sLast": "Último"
-                    },
-            "oAria": {
-                "sSortAscending": ": Ordenar colunas de forma ascendente",
-                "sSortDescending": ": Ordenar colunas de forma descendente"
-                    },
-            "select": {
-                "rows": {
-                    "_": "Selecionado %d linhas",
-                    "0": "Nenhuma linha selecionada",
-                    "1": "Selecionado 1 linha"
-                        }
-                    },
-            "buttons": {
-                "print": "Imprimir",
-                "copy": "Copiar",
-                "copyTitle": "Cópia bem sucedida",
-                "copySuccess": {
-                    "1": "Uma linha copiada com sucesso",
-                    "_": "%d linhas copiadas com sucesso"
-                    }
-                }
-             },
+        
     });
     sessionStorage.setItem('id_usuario', '1')
     sessionStorage.setItem('tipo_usuario', '1')
